c41e36eb6f8d90f5d7396d9a18bdaa1d1d6d55b7
lakshan <manualsplash@gmail.com>
2019-12-20 22:53:09 +0530

    Create http hook

---
+++ src/hooks/http.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export const useHttp = (url, dependencies) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedData, setFetchedData] = useState(null);
+  useEffect(() => {
+    console.log("Calling http hook");
+    setIsLoading(true);
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch.");
+        }
+        return response.json();
+      })
+      .then(data => {
+        setIsLoading(false);
+        setFetchedData(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, dependencies);
+
+  return [isLoading, fetchedData];
+};
