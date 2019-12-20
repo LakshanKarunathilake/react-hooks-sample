608924a2a6ea39cf447a8d8eeb31b5075c353e30
lakshan <manualsplash@gmail.com>
2019-12-20 21:35:18 +0530

    Convert component did mount for useEffect

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,63 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './CharPicker.css';
+import "./CharPicker.css";
 
-class CharPicker extends Component {
-  state = { characters: [], isLoading: false };
+const CharPicker = props => {
+  const [characters, setCharactersState] = useState([]);
+  const [isLoading, setLoadingState] = useState(false);
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    fetch('https://swapi.co/api/people')
+  useEffect(() => {
+    console.log("Working");
+    setLoadingState(true);
+    fetch("https://swapi.co/api/people")
       .then(response => {
         if (!response.ok) {
-          throw new Error('Failed to fetch.');
+          throw new Error("Failed to fetch.");
         }
         return response.json();
       })
       .then(charData => {
         const selectedCharacters = charData.results.slice(0, 5);
-        this.setState({
-          characters: selectedCharacters.map((char, index) => ({
+        setCharactersState(
+          selectedCharacters.map((char, index) => ({
             name: char.name,
             id: index + 1
-          })),
-          isLoading: false
-        });
+          }))
+        );
+        setLoadingState(false);
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  }, []);
 
-  render() {
-    let content = <p>Loading characters...</p>;
+  let content = <p>Loading characters...</p>;
 
-    if (
-      !this.state.isLoading &&
-      this.state.characters &&
-      this.state.characters.length > 0
-    ) {
-      content = (
-        <select
-          onChange={this.props.onCharSelect}
-          value={this.props.selectedChar}
-          className={this.props.side}
-        >
-          {this.state.characters.map(char => (
-            <option key={char.id} value={char.id}>
-              {char.name}
-            </option>
-          ))}
-        </select>
-      );
-    } else if (
-      !this.state.isLoading &&
-      (!this.state.characters || this.state.characters.length === 0)
-    ) {
-      content = <p>Could not fetch any data.</p>;
-    }
-    return content;
+  if (!isLoading && characters && characters.length > 0) {
+    content = (
+      <select
+        onChange={props.onCharSelect}
+        value={props.selectedChar}
+        className={props.side}
+      >
+        {characters.map(char => (
+          <option key={char.id} value={char.id}>
+            {char.name}
+          </option>
+        ))}
+      </select>
+    );
+  } else if (!isLoading && (!characters || characters.length === 0)) {
+    content = <p>Could not fetch any data.</p>;
   }
-}
+  return content;
+};
 
 export default CharPicker;
